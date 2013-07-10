cb29c4f7309b3eec1cd04bc478cf91fb1f0240d8
TheBizzle <jason.bertsche@gmail.com>
2013-07-10 13:30:39 -0500

    Minor refactoring

--- src/controller/PortOpener.scala
+++ src/controller/PortOpener.scala
@@ -11,19 +11,21 @@ private[controller] trait PortOpener {
   self: HasPortsAndStreams with SerialPortEventListener =>
 
   def openPort() {
-    portOpt = portOpt orElse {
+    portOpt = portOpt orElse Option(initializePort())
+  }
+
+  private def initializePort(): SerialPort = {
+    try {
       val port = new SerialPort(portName)
-      try {
-        port.openPort()
-        port.setParams(BAUDRATE_9600, DATABITS_8, STOPBITS_1, PARITY_NONE)
-        port.purgePort(PURGE_RXCLEAR | PURGE_TXCLEAR)
-        port.setEventsMask(MASK_RXCHAR)
-        port.addEventListener(this)
-      }
-      catch {
-        case e: SerialPortException => throw new ExtensionException("Unable to open port " + portName, e)
-      }
-      Option(port)
+      port.openPort()
+      port.setParams(BAUDRATE_9600, DATABITS_8, STOPBITS_1, PARITY_NONE)
+      port.purgePort(PURGE_RXCLEAR | PURGE_TXCLEAR)
+      port.setEventsMask(MASK_RXCHAR)
+      port.addEventListener(this)
+      port
+    }
+    catch {
+      case e: SerialPortException => throw new ExtensionException("Unable to open port " + portName, e)
     }
   }
 
