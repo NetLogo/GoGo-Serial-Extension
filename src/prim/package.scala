d04f54c66c64d372da925f3b511bfb38a2a89fb3
TheBizzle <jason.bertsche@gmail.com>
2013-07-10 14:35:38 -0500

    Removing obsoleted type alias

--- src/prim/package.scala
+++ src/prim/package.scala
@@ -1,10 +1,6 @@
 package org.nlogo.extensions.gogolite
 
 package object prim {
-
-  private[prim] type EE = org.nlogo.api.ExtensionException
-
   private[prim] def buildMask(xs: Seq[_], maskMap: Map[Char, Int]): Int =
     xs.map(_.toString.toLowerCase.head).distinct.map(maskMap).foldLeft(0){ case (acc, x) => acc | x }
-
 }
