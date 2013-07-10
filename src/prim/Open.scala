32e0683d148f68cc8feb044b3d37ae88f8084868
TheBizzle <jason.bertsche@gmail.com>
2013-07-10 14:33:45 -0500

    Doing away with some overly-cautious error-checking

--- src/prim/Open.scala
+++ src/prim/Open.scala
@@ -8,21 +8,12 @@ import
 class Open(manager: ControllerManager) extends DefaultCommand {
   override def getSyntax = Syntax.commandSyntax(Array(Syntax.StringType))
   override def perform(args: Array[Argument], context: Context) {
-
-    import manager.{ getControllerOpt => controllerOpt }
-
     manager.close()
-
-    try {
-      manager.init(args(0).getString)
-      controllerOpt foreach (_.openPort())
+    manager.init(args(0).getString)
+    manager.withController {
+      controller =>
+        controller.openPort()
+        controller.ping()
     }
-    catch {
-      case e: NoClassDefFoundError => throw new EE("Could not initialize GoGo Extension.  Full error message: %s : %s".format(args(0).getString, e.getLocalizedMessage))
-      case e: Exception            => throw new EE("Could not open port %s : %s".format(args(0).getString, e.getLocalizedMessage))
-    }
-
-    controllerOpt map (_.ping())
-
   }
 }
